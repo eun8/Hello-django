745b3f406b8164fbe552ca19f40526056085240a
eun8 <47545659+eun8@users.noreply.github.com>
2019-02-12 12:27:13 +0900

    Heroku 배포 준비

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'se!%_2)c87&xhfev_q=-51a943ilh6ea&wh6fli+t&(mcq8+*x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')

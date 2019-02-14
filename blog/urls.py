048d994694f98168b53c67c24a08f9a574153d30
eun8 <47545659+eun8@users.noreply.github.com>
2019-02-14 09:53:06 +0900

    save

--- blog/urls.py
+++ blog/urls.py
@@ -1,21 +1,30 @@
-"""blog URL Configuration
+from django.urls import path, re_path
+from blog.views import index, hello_times
+from blog.views import articles_by_year
+from blog.views import naver_realtime_keywords
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from django.urls import register_converter
+from blog.converters import FourDigitYearConverter
+from blog.converters import SlugUnicodeConverter
+
+register_converter(FourDigitYearConverter, 'year')
+register_converter(SlugUnicodeConverter, 'slug_unicode')
+
+
+app_name = 'blog'  # URL Reverse 기능을 할 때, 사용.
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('articles/<year:year>/', articles_by_year),
+
+    # re_path('^blog/1/$', post_detail),
+    # re_path('^blog/1/edit/$', post_edit),
+
+    path('', index),
+    # re_path(r'^$', index),
+
+    path('hello_times/<int:times>/', hello_times),
+    # re_path(r'blog/hello_times/(?P<times>\d+)/$', hello_times),
+    
+    path('naver/실시간검색어/', naver_realtime_keywords),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
+def articles_by_year(request, year):
+    return HttpResponse(f'''
+        {year}년도에 대한 목록
+    ''')
+
 
 
 def index(request):
@@ -9,3 +17,12 @@ def index(request):
 def hello_times(request, times):
     message = "안녕하세요" * times
     return HttpResponse(message)
+
+
+def naver_realtime_keywords(request):
+    res = requests.get("http://naver.com")
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    tag_list = soup.select('.PM_CL_realtimeKeyword_rolling .ah_k')
+    text = '<br/>\n'.join([tag.text for tag in tag_list])
+    return HttpResponse(text)
